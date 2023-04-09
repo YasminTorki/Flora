1c5e07742ae1f6421f2c5b6ea0dfc0ff5e052761
mamadmmp <88711223+mamadmmp@users.noreply.github.com>
2023-04-10 00:43:31 +0330

    Add files via upload

---
+++ backend/app/http/validators/user/auth.schema.js
@@ -0,0 +1,17 @@
+const Joi = require("@hapi/joi");
+const authSchema = Joi.object({
+  name: Joi.string().required(),
+  Lastname: Joi.string().required(),
+  email: Joi.string().lowercase().trim().email().required().messages({
+    "any.required": "ایمیل الزامی است",
+    "string.email": "ایمیل وارد شده باید صحیح باشد",
+  }),
+  password: Joi.string().min(6).trim().required().messages({
+    "any.required": "وارد کردن پسورد الزامی است",
+    "string.min": "طول کلمه عبور حداقل شش کاراکتر است",
+  }),
+}).options({ abortEarly: false });
+
+module.exports = {
+  authSchema,
+};
