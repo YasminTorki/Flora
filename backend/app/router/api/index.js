eb21a9d45066858eee89f25ef37c7e7935c898dd
Yasmin <89246022+YasminTorki@users.noreply.github.com>
2023-04-10 00:51:13 +0330

    Add files via upload

---
+++ backend/app/router/api/index.js
@@ -0,0 +1,25 @@
+const homeController = require("../../http/controllers/api/home.controller");
+const router = require("express").Router();
+/**
+ * @swagger
+ * tags:
+ *  name: IndexPage
+ *  description : index page routes and data
+ */
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: index of routes
+ *      tags: [IndexPage]
+ *      description: get all need data for index-page 
+ *      responses:
+ *          200: 
+ *              description: success 
+ *          404:
+ *              description: notfound
+ */
+router.get("/", homeController.indexPage);
+module.exports = {
+    AllRoutes : router
+}
